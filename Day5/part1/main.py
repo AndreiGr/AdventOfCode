ca5b8325651239fe68c7e49259051ad36fa70389
Andrei Grigoriu <andrei.grigoriu@cegeka>
2023-12-05 13:04:11 +0200

    Day5 Part1 solution

--- Day5/part1/main.py
+++ Day5/part1/main.py
@@ -1,7 +1,29 @@
 import os
+import sys
 FILE_PATH = os.path.dirname(os.path.abspath(__file__))
 INPUT_FILE_PATH = os.path.join(FILE_PATH, "../data/input.txt")
 TEST_FILE_PATH = os.path.join(FILE_PATH, "../data/test.txt")
+
+class Seed:
+    def __init__(self, number, soil=-1, fertilizer=-1, water=-1, light=-1, temperature=-1, humidity=-1, location=-1):
+        self.number = int(number)
+        self.soil = int(soil)
+        self.fertilizer = int(fertilizer)
+        self.water = int(water)
+        self.light = int(light)
+        self.temperature = int(temperature)
+        self.humidity = int(humidity)
+        self.location = int(location)
+    
+    def __str__(self):
+        return f"Seed {self.number}, soil {self.soil}, fertilizer {self.fertilizer}, water {self.water}, light {self.light}, temperature {self.temperature}, humidity {self.humidity}, location {self.location}"
+
+class Map:
+    def __init__(self, source, destination, lrange):
+        self.source = int(source)
+        self.destination = int(destination)
+        self.lrange = int(lrange)
+        self.delta = int(destination)-int(source)
 
 seeds = []
 seedtosoil = []
@@ -15,35 +37,57 @@ humiditytolocation = []
 def processseeds():
     for seed in seeds:
         for element in seedtosoil:
-            if seed.seed >= element.sourcestart and seed < element.sourcestart+element.range:
-                seed.soil = seed.seed + element.delta
+            if seed.number >= element.source and seed.number < element.source+element.lrange:
+                seed.soil = seed.number + element.delta
+        if seed.soil == -1:
+            seed.soil = seed.number
 
-        
+        for element in soiltofertilizer:
+            if seed.soil >= element.source and seed.soil < element.source+element.lrange:
+                seed.fertilizer = seed.soil + element.delta
+        if seed.fertilizer == -1:
+            seed.fertilizer = seed.soil
+
+        for element in fertilizertowater:
+            if seed.fertilizer >= element.source and seed.fertilizer < element.source+element.lrange:
+                seed.water = seed.fertilizer + element.delta
+        if seed.water == -1:
+            seed.water = seed.fertilizer
+
+        for element in watertolight:
+            if seed.water >= element.source and seed.water < element.source+element.lrange:
+                seed.light = seed.water + element.delta
+        if seed.light == -1:
+            seed.light = seed.water
+
+        for element in lighttotemperature:
+            if seed.light >= element.source and seed.light < element.source+element.lrange:
+                seed.temperature = seed.light + element.delta
+        if seed.temperature == -1:
+            seed.temperature = seed.light
+
+        for element in temperaturetohumidity:
+            if seed.temperature >= element.source and seed.temperature < element.source+element.lrange:
+                seed.humidity = seed.temperature + element.delta
+        if seed.humidity == -1:
+            seed.humidity = seed.temperature
+
+        for element in humiditytolocation:
+            if seed.humidity >= element.source and seed.humidity < element.source+element.lrange:
+                seed.location = seed.humidity + element.delta
+        if seed.location == -1:
+            seed.location = seed.humidity
 
 def processmap(line,map):
-    map.append({
-        "sourcestart" : line[1],
-        "destinationstart" : line[0],
-        "range" : line[2],
-        "delta" : int(line[0]) - int(line[1])
-    })
+    map.append(Map(line[1],line[0],line[2]))
 
 def readseeds(line):
-    for seed in line:
-        seeds.append({
-            "seed" : seed,
-            "soil" : -1,
-            "fertilizer" : -1,
-            "water" : -1,
-            "light": -1,
-            "temperature": -1,
-            "humidity": -1,
-            "location": -1
-        })
+    for element in line:
+        seeds.append(Seed(element))
 
 def main():
-    with open(TEST_FILE_PATH,"r") as file:
-    # with open(INPUT_FILE_PATH,"r") as file:
+    # with open(TEST_FILE_PATH,"r") as file:
+    with open(INPUT_FILE_PATH,"r") as file:
         lines = file.readlines()
 
     isseedtosoil = False
@@ -137,7 +181,14 @@ def main():
         elif(ishumiditytolocation and line.strip() != ""):
             processmap(line.strip().split(" "),humiditytolocation)
     
-    print("Read")
+    processseeds()
+
+    location = sys.maxsize
+    for seed in seeds:
+        if seed.location < location:
+            location = seed.location
+
+    print(location)
     
 
 
